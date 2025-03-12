844461315fc957604a301eecaf8a2d136002cafb
mr-hoffman <hoffmant@hilbertk12.org>
2025-03-12 09:33:55 -0500

    Create guess-who.py

---
+++ guess-who.py
@@ -0,0 +1,26 @@
+################### Character Dictonary ################### 
+characters = {
+  
+} 
+
+##################### Game Functions ###################### 
+
+def list():
+  pass
+
+def ask(trait):
+  pass
+
+def guess(user_guess):
+  pass
+
+def help():
+  pass
+
+def quit():
+  pass
+
+
+####################### Game Loop ######################## 
+
+
